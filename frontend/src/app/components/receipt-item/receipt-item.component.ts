6a8835e3bac9e79ddf33759bf35df406bfa27424
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-21 17:34:29 -0400

    big commit, allows for editing added receipt items

--- frontend/src/app/components/receipt-item/receipt-item.component.ts
+++ frontend/src/app/components/receipt-item/receipt-item.component.ts
@@ -1,12 +1,11 @@
 import {
   Component,
   EventEmitter,
-  Inject,
   Input,
   OnInit,
   Output,
 } from "@angular/core";
-import { Observable, of } from "rxjs";
+import { v4 as uuid } from 'uuid';
 import { ReceiptError, ReceiptItem } from "src/app/model";
 import { NotificationService } from "src/app/services";
 
@@ -24,9 +23,10 @@ export class ReceiptItemComponent implements OnInit {
     // decide which styling is needed based on if this ReceiptItem component is displaying an added item or the template
     this.inputFieldStyles = this.receiptItem ? "added-item-style" : "template-style";
 
-    // set the boolean observable attribute that keeps track of whether or not the added receipt item is being edited
-    // this.editingReceiptItem = of(!!this.receiptItem); // originally using an observable here
-    this.editingReceiptItem = !!this.receiptItem;
+    // this is bad logic, it works but i have a double negative i think which makes this hard to read
+    // in the html the two input fields disabled state is set to the opposite of this.isEditing
+    // so if this.isEditing === false then the fields are disabled, if this.isEditing === true then the fields are not disabled
+    this.isEditing = this.receiptItem ? false : true;
   }
 
   // this is used for displaying an already added ReceiptItem
@@ -37,10 +37,13 @@ export class ReceiptItemComponent implements OnInit {
   // this emits when a new ReceiptItem is created/add
   @Output()
   public receiptItemEmitter = new EventEmitter<ReceiptItem>();
+
+  @Output()
+  public editReceiptItemEmitter = new EventEmitter<ReceiptItem>();
   
   // keeps track of a ReceiptItem and if it is being edited or not
   // public editingReceiptItem: Observable<boolean> = new Observable<boolean>(); // originally using an observable here
-  public editingReceiptItem: boolean = false;
+  public isEditing: boolean = true;
 
   // keeps track of the styling needed on the input fields
   public inputFieldStyles: string = "";
@@ -60,14 +63,38 @@ export class ReceiptItemComponent implements OnInit {
     } else {
       // emites the newly created ReceiptItem to the parent component "ReceiptItemList"
       this.receiptItemEmitter.emit({
+        id: uuid(),
         shortDescription: shortDescription,
         price: price,
       });
     }
   }
 
-  public editReceiptItem(): void {
-    // this.editingReceiptItem = of(!this.editingReceiptItem); // originally using an observable here
-    this.editingReceiptItem = !this.editingReceiptItem;
+  public editReceiptItem(
+    shortDescription: string,
+    price: string
+  ): void {
+    if (!this.receiptItem || !shortDescription || !price) {
+      this.notificationService.setNotification(
+        ReceiptError.EditReceiptItemError,
+      );
+      return;
+    }
+
+    const hasBeenEdited = 
+      this.receiptItem.shortDescription !== shortDescription || 
+      this.receiptItem.price !== price;
+
+    // if edits were made then we want to emit those changes up
+    if (this.isEditing && hasBeenEdited) {
+      this.receiptItem = {
+        id: this.receiptItem.id,
+        shortDescription: shortDescription,
+        price: price,
+      };
+      this.editReceiptItemEmitter.emit(this.receiptItem);
+    }
+
+    this.isEditing = !this.isEditing;
   }
 }
